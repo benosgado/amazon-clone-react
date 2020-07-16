51ab52b3cae5690ef7c711b573d16227e69c5911
rohit <rtdkumar@gmail.com>
2020-07-15 23:09:47 -0500

    created a Products, Product component

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,26 +2,59 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
     <nav className="header">
-      {/* logo on the left - > img tag*/}
       <Link to="/">
         <img
           alt=""
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
+        {/* <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/gno/sprites/nav-sprite-global_bluebeacon-2x_optimized_layout1._CB468670774_.png"
+          alt=""
+        /> */}
       </Link>
 
-      {/* Search box */}
       <div className="header__search">
         <input type="text" className="header__searchInput" />
-        <searchIcon className="header__searchIcon" />
+        <SearchIcon className="header__searchIcon" />
       </div>
-      {/* 3 links */}
-      {/* Basket with a number */}
+
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Rohit</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            {/* Shopping basket icon */}
+            <ShoppingBasketIcon></ShoppingBasketIcon>
+            {/* Number of items in the basket */}
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }
---
+++ amazon-clone/src/Products.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Product from "./Product";
+
+function Products() {
+  let style = {
+    display: "flex",
+  };
+  let Products = [
+    {
+      id: 121,
+      title: "Vans Men's Classic Short Sleeve T-Shirt",
+      price: 20.72,
+      rating: 4,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/711YeFGUlfL._AC_UY879_.jpg",
+    },
+    {
+      id: 122,
+      title: "Levi's Men's Graphic Set-in Neck T-Shirt",
+      price: 11.96,
+      rating: 5,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/815KnQvZrcL._AC_UY879_.jpg",
+    },
+    {
+      id: 123,
+      title: "Eastpak Tranverz S Suitcase, 51 cm, 42 L, Black",
+      price: 121.01,
+      rating: 3,
+      image: "https://m.media-amazon.com/images/I/71-09J9aqUL._AC_UY218_.jpg",
+    },
+    {
+      id: 124,
+      title: "Vans M CLASSIC CREW - Socks - Men (Pack of 3)",
+      price: 17.6,
+      rating: 3,
+      image: "https://m.media-amazon.com/images/I/919XV3IzIiL._AC_UY218_.jpg",
+    },
+    {
+      id: 125,
+      title:
+        "Samsung QN82Q60RAFXZA Flat 82-Inch QLED 4K Q60 Series (2019) Ultra HD Smart TV with HDR and Alexa Compatibility",
+      price: 1997.99,
+      rating: 5,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/91uohwV%2Bk3L._AC_SX679_.jpg",
+    },
+  ];
+
+  return (
+    <div style={style}>
+      {Products.map((product) => (
+        <Product
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          rating={product.rating}
+          image={product.image}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Products;
